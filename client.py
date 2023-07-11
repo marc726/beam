1bec7e908c3f374d1a12df7fbe019674c761af3f
marc726 <marcrizzolo726@gmail.com>
2023-07-11 18:24:42 -0400

    error handling

--- client.py
+++ client.py
@@ -9,7 +9,6 @@ BUFFER_SIZE = 8192
 
 class Client:
     def __init__(self, host='localhost', port=12345, save_dir=''):
-        
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
         self.port = port
@@ -32,31 +31,31 @@ class Client:
         self.client_socket.close()
         self.is_connected = False
 
-def receive_file(self):
-    try:
-        # Receive the first message from the server
-        message = self.client_socket.recv(BUFFER_SIZE)
+    def receive_file(self):
+        try:
+            # Receive the first message from the server
+            message = self.client_socket.recv(BUFFER_SIZE)
 
-        if message == b'No file selected.':
-            print('Server did not select a file.')
-            return
+            if message == b'No file selected.':
+                print('Server did not select a file.')
+                return
 
-        # If the message is not 'No file selected.', it is the filename
-        filename = message.decode().strip()
+            # If the message is not 'No file selected.', it is the filename
+            filename = message.decode().strip()
 
-        # Open the file
-        file_path = os.path.join(self.save_dir, filename)
-        with open(file_path, 'wb') as file:
-            while True:
-                data = self.client_socket.recv(BUFFER_SIZE)
-                if not data:
-                    break
-                file.write(data)
-        print('File received.')
-    except Exception as e:
-        print(f"Error occurred: {e}")
-    finally:
-        self.disconnect_from_server()
+            # Open the file
+            file_path = os.path.join(self.save_dir, filename)
+            with open(file_path, 'wb') as file:
+                while True:
+                    data = self.client_socket.recv(BUFFER_SIZE)
+                    if not data:
+                        break
+                    file.write(data)
+            print('File received.')
+        except Exception as e:
+            print(f"Error occurred: {e}")
+        finally:
+            self.disconnect_from_server()
 
     def disconnect_from_server(self):
         self.is_connected = False
