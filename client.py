147fdbdfca6a95c176244af39f346e929b9a4b6a
marc726 <marcrizzolo726@gmail.com>
2023-07-11 18:35:46 -0400

    progress bar proto

--- client.py
+++ client.py
@@ -32,27 +32,37 @@ class Client:
 
     def receive_file(self):
         try:
-            # Receive filename
-            filename = self.client_socket.recv(BUFFER_SIZE).decode().strip()
-            if filename == 'No file selected.':
+            # Receive the first message from the server
+            message = self.client_socket.recv(BUFFER_SIZE)
+
+            if message == b'No file selected.':
                 print('Server did not select a file.')
                 return
-            
+
+            # If the message is not 'No file selected.', it is the filename
+            filename = message.decode().strip()
+
             # Receive file size
-            file_size = int(self.client_socket.recv(BUFFER_SIZE).decode().strip())
-            
+            file_size = ""
+            while True:
+                char = self.client_socket.recv(1).decode()
+                if char == '\n':
+                    break
+                file_size += char
+            file_size = int(file_size)
+
             # Open the file
             file_path = os.path.join(self.save_dir, filename)
-            received_size = 0
             with open(file_path, 'wb') as file:
-                while received_size < file_size:
-                    data = self.client_socket.recv(min(BUFFER_SIZE, file_size - received_size))
+                bytes_received = 0
+                while bytes_received < file_size:
+                    data = self.client_socket.recv(BUFFER_SIZE)
                     if not data:
                         break
-                    received_size += len(data)
+                    bytes_received += len(data)
                     file.write(data)
-                    self.print_progress_bar(received_size, file_size)
-            print('\nFile received.')
+                    self.print_progress_bar(bytes_received, file_size)
+            print('File received.')
         except Exception as e:
             print(f"Error occurred: {e}")
         finally:
